a5d3765e1242a331a92f8f65978725eea4add586
박지현 <102128060+wlgusqkr@users.noreply.github.com>
2025-12-22 19:30:21 +0900

    hotfix: Config api server url fix

--- today-s-sound/Core/Network/Config.swift
+++ today-s-sound/Core/Network/Config.swift
@@ -6,7 +6,7 @@ enum Config {
       return "https://www.today-sound.com"
 //      return "http://localhost:8080" // 개발 서버
     #else
-      return "https://api.todays-sound.com" // 운영 서버
+      return "https://www.today-sound.com"
     #endif
   }
 }
